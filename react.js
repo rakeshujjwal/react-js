3e72985e0c498276b5afd8e5fe76f55c1dbc5dc3
ujjwalskyhunter <72217976+ujjwalskyhunter@users.noreply.github.com>
2022-10-16 12:24:15 +0530

    Update react.js

--- react.js
+++ react.js
@@ -1,17 +1,1 @@
 
-
-function freeze_gif(i,j) {
-  var wid = j;
-  var c = document.createElement('canvas');
-  var w = c.width = i.width;
-  var h = c.height = i.height;
-  c.getContext('2d').drawImage(i, 0, 0, w, h);
-  try {
-    i.src = c.toDataURL("image/gif"); 
-  } catch(e) { 
-    for (var j = 0, a; a = i.attributes[j]; j++)
-      c.setAttribute(a.name, a.value);
-    i.parentNode.replaceChild(c, i,j);
-  }
-}
-
